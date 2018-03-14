0f50e610ba0f9cc92094f92a15f6b9352e6f970b
jasenmichael <jasen@ready-builder.com>
2018-03-14 12:10:47 -0600

    added array of numbers and a function to add them

---
+++ app/main.js
@@ -0,0 +1,9 @@
+let firstNumArr = [ 1, 4, 35, 6, 7, 79, 6, 3, 42, 14 ]
+
+function summArr() {
+    var total = 0
+    for(i=0; i < firstNumArr.length; i++){
+        total += firstNumArr[i]
+    }
+    return total
+}
